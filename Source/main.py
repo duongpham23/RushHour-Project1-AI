18b166ac24a2174df895156dc4f84074795d17c0
Tin Nguyen <156284579+tinnguyen0706@users.noreply.github.com>
2025-06-26 22:47:34 +0700

    Add function load_map

--- Source/main.py
+++ Source/main.py
@@ -1,1 +1,13 @@
+import numpy as np
+import json
 
+def load_map(file_path: str):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        raw_data = json.load(file)
+
+    maps = []
+    for map_data in raw_data:
+        map_array = np.array(map_data["data"], dtype=int)
+        maps.append(map_array)
+
+    return maps
