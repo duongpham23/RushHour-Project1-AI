a7fe30fd0fa8f7e1e44e60c6db1d8d3bb5793e11
duongpham23 <146987024+duongpham23@users.noreply.github.com>
2025-06-30 22:15:44 +0700

    Update main.py

--- Source/main.py
+++ Source/main.py
@@ -1,13 +1,1 @@
-import numpy as np
-import json
-
-def load_map(file_path: str):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        raw_data = json.load(file)
-
-    maps = []
-    for map_data in raw_data:
-        map_array = np.array(map_data["data"], dtype=int)
-        maps.append(map_array)
-
-    return maps
+# Viet lai ham load map nha Tin. Dung LIST nhe
