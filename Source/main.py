8ffa9e6564ea95651a936cad70fee52d113d2e6a
Tin Nguyen <156284579+tinnguyen0706@users.noreply.github.com>
2025-07-01 22:54:09 +0700

    Add load_map

--- Source/main.py
+++ Source/main.py
@@ -1,1 +1,18 @@
 # Viet lai ham load map nha Tin. Dung LIST nhe
+import json
+import os
+
+def load_map(file_path: str):
+    # Đường dẫn đến thư mục hiên tại
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    # Kết hợp đường dẫn thư mục với tên tệp
+    file_path = os.path.join(dir_path, file_path)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        raw_data = json.load(file)
+
+    maps = []
+    for map_data in raw_data:
+        map_array = map_data["data"]
+        maps.append(map_array)
+
+    return maps
